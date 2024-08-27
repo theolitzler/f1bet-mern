5fe40f895322e6506155e475ee3465282f9c7076
Théo Litzler <theolitzler@gmail.com>
2024-08-27 15:15:33 -0400

    Cleaned

--- client/src/pages/RaceDetails.jsx
+++ client/src/pages/RaceDetails.jsx
@@ -1,36 +1,40 @@
-import { useParams } from 'react-router-dom';
 import DriverList from '../components/DriverList.jsx';
 import Navbar from "../components/Navbar.jsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const RaceDetails = () => {
-  const { raceId } = useParams();
+  const [raceInfo, setRaceInfo] = useState()
 
   useEffect(() => {
     const fetchRace = async () => {
       try {
-        const response = await fetch('http://localhost:9000/drivers/all');
+        const raceId = location.pathname.split("/").pop();
+        const response = await fetch(`http://localhost:9000/races/${raceId}`);
         const data = await response.json();
 
         if (data.success) {
-          setDriverList(data.message);
+          setRaceInfo(data.message);
         } else {
-          console.error('Failed to fetch drivers:', data.message);
+          console.error('Failed to fetch race info:', data.message);
         }
       } catch (error) {
-        console.error('Error fetching drivers:', error);
+        console.error('Error fetching race info:', error);
       }
     };
 
     fetchRace();
   }, []);
 
+  if (!raceInfo) {
+    return <p>Loading...</p>; // Handle loading state
+  }
+
   return (
     <div className="race-details max-w-4xl mx-auto">
     <Navbar />
       <header className="race-header">
         {/* Display basic race information here */}
-        <h1>Race Details for {raceId}</h1>
+        <h1 className="p-4">{raceInfo.name}</h1>
       </header>
       <DriverList />
     </div>
