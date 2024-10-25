5def102f6045d5b5a68129682229d33799983483
Théo Litzler <theolitzler@gmail.com>
2024-10-25 16:04:48 -0400

    New backend

--- server/routes/DriverRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const driverRouter = express.Router();
-const driverController = require("../controllers/DriverController");
-
-driverRouter.get("/all", driverController.getAllDrivers);
-
-module.exports = driverRouter;
